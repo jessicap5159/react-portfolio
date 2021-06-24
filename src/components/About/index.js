bfe5674972d724a4e78e41c5bb69f4eb6cb14485
jessicap5159 <jessicaj5159@gmail.com>
2021-06-24 14:56:35 -0700

    added contact, about components

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import coverImage from "../../assets/cover/cover-image.jpg";
+// import my own photo
+
+function About() {
+
+  return (
+    <section className="my-5" >
+      <h1 id="about">Who is Jessica?</h1>
+      {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
+      <div className="my-2">
+        <p>
+            Add more text here later...
+      </p>
+      </div>
+    </section>
+  )
+}
+
+export default About;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,7 +1,24 @@
 function Nav() {
 
     return (
-      <header>
+        <header>
+        <h2>
+          <a href="/">
+            <span role="img" aria-label="wave"> &#128075;</span> Welcome!
+          </a>
+        </h2>
+        <nav>
+          <ul className="flex-row">
+            <li className="mx-2">
+              <a href="#about">
+                About me
+              </a>
+            </li>
+            <li>
+              <span>Contact</span>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
