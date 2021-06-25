32e1e8bd2c20ddc2791945ee8155f4438bca7aba
jessicap5159 <jessicaj5159@gmail.com>
2021-06-25 12:20:30 -0700

    finished basic nav bar links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Nav from './components/Navigation';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
-        
+        <About></About>
       </main>
     </div>
   );
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,27 +1,31 @@
 function Nav() {
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="wave">
+            {" "}
+            &#128075;
+          </span>{" "}
+          Welcome!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">About me</a>
+          </li>
+          <a href="#Portfolio">Portfolio</a>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#Resume">Résumé</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
-    return (
-        <header>
-        <h2>
-          <a href="/">
-            <span role="img" aria-label="wave"> &#128075;</span> Welcome!
-          </a>
-        </h2>
-        <nav>
-          <ul className="flex-row">
-            <li className="mx-2">
-              <a href="#about">
-                About me
-              </a>
-            </li>
-            <li>
-              <span>Contact</span>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Nav;
-  
+export default Nav;
