60386ce3c2b6ea948efc04d8ca00a7cc8ac966a1
jessicap5159 <jessicaj5159@gmail.com>
2021-07-13 11:04:26 -0700

    adding new project to project page and updating README

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,10 @@ function About() {
       <div className="text">
         <h1 className="fancy-header">Who Am I?</h1>
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, rutrum quis nunc ac, egestas porta felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam feugiat nisl ac lectus faucibus pellentesque. Integer ac ante et felis ornare scelerisque. Suspendisse nunc sapien, vestibulum eget consectetur nec, consequat vitae nisl. Fusce odio nulla, suscipit in gravida id, blandit ac velit. Ut vestibulum ipsum eget purus vulputate, a tempor odio fringilla. Nulla molestie hendrerit est id laoreet. Fusce ut mi non ante tincidunt egestas sed vel sem. Morbi venenatis odio felis, vitae convallis sapien rhoncus eget. Duis suscipit condimentum diam quis hendrerit.
+          My name is Jessica Pyne. I was born in London, England, but moved to the U.S. as a small child, where I have lived ever since. 
+          I graduated from UC Santa Cruz in 2012 with a degree in psychology, and have also studied a number of languages, including Spanish, Greek, Hebrew, and Latin. 
+          I began to code in 2020, when I realized that I could apply my language skills to web development. I hope to be able to use these skills to support things that are important to me. Please contact me 
+          with any questions, or if you'd like to work on something together!
       </p>
       
     </div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,12 +5,22 @@ import budget from "../../images/budgettracker.png"
 import note from "../../images/notetaker.png";
 import passwordgen from "../../images/passwordgenerator.png";
 import scheduler from "../../images/superscheduler.png";
-import weather from "../../images/wonderfulweather.png"
+import weather from "../../images/wonderfulweather.png";
+import proTeam from '../../images/pro-team.png'
+
 
 function Portfolio() {
   const [dummyData, setDummyData] = useState([
     {
       description: "project1",
+      title: "Pro-Team",
+      image:  proTeam,
+      gitHubLink: "https://github.com/Xunaxidonita/Pro-Team",
+      projectLink: "http://agile-shore-63107.herokuapp.com/",
+    },
+
+    {
+      description: "project2",
       title: "What's for Dinner?",
       image:  dinner,
       gitHubLink: "https://github.com/Xunaxidonita/Whats_for_dinner",
@@ -18,21 +28,21 @@ function Portfolio() {
     },
 
     {
-      description: "project2",
+      description: "project3",
       title: "Budget Tracker",
       image: budget,
       gitHubLink: "https://github.com/jessicap5159/budget-tracker",
       projectLink: "https://ancient-island-34751.herokuapp.com/"
     },
     {
-      description: "project3",
+      description: "project4",
       title: "Note Taker",
       image: note,
       gitHubLink: "https://github.com/jessicap5159/note-taker",
       projectLink: "https://infinite-plains-29071.herokuapp.com/"
     },
     {
-      description: "project4",
+      description: "project5",
       title: "Password Generator",
       image: passwordgen,
       gitHubLink: "https://github.com/jessicap5159/awesome-password-generator",
@@ -40,7 +50,7 @@ function Portfolio() {
     },
 
     {
-      description: "project5",
+      description: "project6",
       title: "Super Scheduler",
       image: scheduler,
       gitHubLink: "https://github.com/jessicap5159/superscheduler",
@@ -48,7 +58,7 @@ function Portfolio() {
     },
 
     {
-      description: "project6",
+      description: "project7",
       title: "Wonderful Weather App",
       image: weather,
       gitHubLink: "https://github.com/jessicap5159/wonderfulweatherapp",
